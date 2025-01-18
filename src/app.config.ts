5887bc8c3cf307dcff6774711fe7542963dacee7
Jerald Golden <jeraldgolden00@gmail.com>
2025-01-18 18:13:09 +0530

    refactor: remove server control HTML and clean up app configuration

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,4 @@
 import config from "@colyseus/tools";
-import { auth } from "@colyseus/auth";
-import path from 'path';
-import express from 'express';
 
 import { RoomHandler } from "./rooms/room-handler";
 
@@ -18,18 +15,5 @@ export default config({
         gameServer.onShutdown(function () {
             console.log(`game server is going down.`);
         });
-    },
-
-    initializeExpress: (app) => {
-        app.use(auth.prefix, auth.routes());
-
-        app.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, '../public/server-control.html'));
-        });
-
-        app.use(express.static(path.join(__dirname, '../public')));
-    },
-
-    beforeListen: () => {
     }
 });
